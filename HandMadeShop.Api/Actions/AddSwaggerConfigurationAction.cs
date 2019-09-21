323d6d87c86b50cfa70b34f35aa53a40d9381409
Vitaliy Grigorenko <vitaliy.grigorenko@outlook.com>
2019-09-21 20:59:39 +0300

    refactoring

--- HandMadeShop.Api/Actions/AddSwaggerConfigurationAction.cs
+++ HandMadeShop.Api/Actions/AddSwaggerConfigurationAction.cs
@@ -1,23 +1,21 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace HandMadeShop.Api.Actions
 {
   public static class AddSwaggerConfigurationAction
   {
-    public static void AddSwaggerConfiguration(this IServiceCollection services, IConfiguration configuration)
+    public static void AddSwaggerConfiguration(this IServiceCollection services)
     {
-        services.AddSwaggerDocument(config =>
-        {
-            config.PostProcess = document =>
-            {
-                document.Info.Version = "v1";
-                document.Info.Title = "HandMadeShop API";
-                document.Info.Description = "Best hand made shop ever developed by professionals";
-                document.Info.TermsOfService = "None";
-            };
-        });
+      services.AddSwaggerDocument(config =>
+      {
+        config.PostProcess = document =>
+          {
+            document.Info.Version = "v1";
+            document.Info.Title = "HandMadeShop API";
+            document.Info.Description = "Best hand made shop ever developed by professionals";
+            document.Info.TermsOfService = "None";
+          };
+      });
     }
-
   }
 }
--- HandMadeShop.Api/Actions/UseSwaggerConfigurationAction.cs
+++ HandMadeShop.Api/Actions/UseSwaggerConfigurationAction.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace HandMadeShop.Api.Actions
 {
   public static class UseSwaggerConfigurationAction
   {
-    public static void UseSwaggerConfiguration(this IApplicationBuilder applicationBuilder, IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+    public static void UseSwaggerConfiguration(this IApplicationBuilder applicationBuilder)
     {
         applicationBuilder.UseOpenApi();
         applicationBuilder.UseSwaggerUi3();
     }
-
   }
 }
